1200841790fa05069efb8bd8db55188285495848
proloypaul <proloyp23@gmail.com>
2021-10-15 11:30:42 +0600

    modify json

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,7 +10,7 @@ const Shop = () => {
 
     const [products, setProducts] = useState([])
     useEffect(() => {
-        const url = `../products.JSON`
+        const url = `../products.json`
         fetch(url)
             .then(res => res.json())
             .then(data =>{
