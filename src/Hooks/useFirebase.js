576cbbc7dfe891185b04d6f7591cd41fa4313d93
proloypaul <proloyp23@gmail.com>
2021-10-17 14:46:53 +0600

    firebase deploy

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -42,11 +42,14 @@ const useFirebase = () => {
 
     // logical signIn and signOut process
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
             if(user){
                 setUser(user)
+            }else{
+                setUser({})
             }
         })
+        return () => unsubscribed
     }, [])
 
 
